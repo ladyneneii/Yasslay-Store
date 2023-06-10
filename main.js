e954c426625873406d383724d3fd2793be4915ab
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-06-10 12:38:23 +0800

    [feature] Added increment, decrement, update, and calculation functions

--- main.js
+++ main.js
@@ -27,7 +27,9 @@ let shopItemsData = [{
     price: 1200,
     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti cumque dolores",
     img: "images/dress.avif"
-}]
+}];
+
+let basket = [];
 
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x) => {
@@ -41,9 +43,9 @@ let generateShop = () => {
                 <div class="price-quantity">
                     <h2>₱ ${price}</h2>
                     <div class="buttons">
-                        <i class="bi bi-dash-lg"></i>
-                        <div class="quantity">0</div>
-                        <i class="bi bi-plus-lg"></i>
+                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                        <div id=${id} class="quantity">0</div>
+                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                     </div>
                 </div>
             </div>
@@ -53,3 +55,46 @@ let generateShop = () => {
 };
 
 generateShop();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem.id,
+            item: 1
+        });
+    } else {
+        search.item++;
+    }
+
+    update(selectedItem.id);
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search.item === 0) {
+        return;
+    } else {
+        search.item--;
+    }
+
+    update(selectedItem.id);
+};
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+
+    document.getElementById(id).innerHTML = search.item;
+
+    calculation();
+};
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartNumber");
+
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
