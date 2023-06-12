4a2aab529ed2b8137b44b3f3be782f71ea2c7719
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-06-12 12:35:09 +0800

    Added total amount per item, total amount overall, fixed bugs, and finalised everything

--- src/cart.js
+++ src/cart.js
@@ -15,19 +15,26 @@ let generateCartItems = () => {
         return (ShoppingCart.innerHTML = basket.map((x) => {
             let { id, item } = x;
             let search = shopItemsData.find((y) => y.id === id) || [];
+            let { img, name, price } = search;
             return `
                 <div class="cart-item">
-                    <img class="image" src=${search.img} alt="" />
+                    <img class="cart-image" src=${img} alt="" />
                     <div class="details">
                         <div class="title-price-x">
-                            <h4>
-                                <p>${search.name}</p>
-                                <p>₱ ${search.price}</p>
+                            <h4 class="title-price">
+                                <p>${name}</p>
+                                <p class="cart-item-price">₱ ${price}</p>
                             </h4>
-                            <i class="bi bi-x-lg"></i>
+                            <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
                         </div>
-                        <div class="cart-buttons"></div>
-                        <h3></h3>
+
+                        <div class="buttons">
+                            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                            <div id=${id} class="quantity">${item}</div>
+                            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                        </div>
+
+                        <h3>₱ ${item * search.price}</h3>
                     </div>
                 </div>
             `;
@@ -44,3 +51,80 @@ let generateCartItems = () => {
 }
 
 generateCartItems();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id); // searches the  item in the basket to see if the item already exists in the basket
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem.id,
+            item: 1
+        });
+    } else {
+        search.item++;
+    }
+
+    generateCartItems(); 
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search === undefined || search.item === 0) {
+        return;
+    } else {
+        search.item--;
+    }
+
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0); // removes item from the basket when its quantity turns 0
+    generateCartItems(); // re renders the cards from the basket so that the items with 0 quantity are removed
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+
+    document.getElementById(id).innerHTML = search.item;
+
+    totalAmount();
+    calculation();
+};
+
+let removeItem = (id) => {
+    let selectedItem = id;
+
+    basket = basket.filter((x) => x.id !== selectedItem.id);
+    totalAmount();
+    generateCartItems();
+    calculation();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let totalAmount = () => {
+    if (basket.length !== 0) {
+        let amount = basket.map((x) => {
+            let { item, id } = x;
+            let search = shopItemsData.find((y) => y.id === id) || [];
+            return item * search.price;
+        }).reduce((x, y) => x + y, 0);
+        label.innerHTML = `
+            <h2>Total Bill: ₱ ${amount}</h2>
+            <button class="checkout">Checkout</button>
+            <button onclick="clearCart()" class="removeAll">Clear cart</button>
+        `;
+    } else return;
+};
+
+totalAmount();
+
+let clearCart = () => {
+    basket = [];
+    generateCartItems();
+    calculation();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
